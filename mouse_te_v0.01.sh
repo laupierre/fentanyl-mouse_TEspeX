a3f50538b02fb437bfe12e1a213e3d71bf13da7b
laupierre <laupierre@users.noreply.github.com>
2023-03-29 16:47:28 +0200

    Create mouse_te_v0.01.sh

---
+++ mouse_te_v0.01.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#### This is the TEspeX analysis for the mouse species (v.0.1dev)
+
+
+#################
+#### Variables (this folder is not bound by apptainer)
+CONTAINER=/projects/ncrrbt_share_la/dev_pipe
+INDEX=/projects/ncrrbt_share_la/dev_pipe
+CPUS=12
+
